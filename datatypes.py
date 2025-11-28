bb66a3ccd6515812a0878f4336a025276bb6328f
deekshithadasareddy <dasareddydeekshitha@gmail.com>
2025-11-28 17:58:41 +0530

    Add datatypes.py with examples of Python types

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+# Different data types in Python
+a = 10
+b = 3.14
+c = "Hello Python"
+d = True
+e = [1, 2, 3]
+f = (4, 5, 6)
+g = {"name": "Deekshi", "age": 21}
+h = {1, 2, 3, 4}
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+print(h, type(h))
